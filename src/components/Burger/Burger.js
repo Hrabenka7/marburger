5e032422ab6292c045bee22489d347f060b46d25
Marketa Skoloudikova <marketa.skoloudikova@forcemanager.net>
2018-08-22 18:18:07 +0200

    output ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,14 +3,30 @@ import burgerStyling from './Burger.css'
 import Ingredient from './Ingredient/Ingredient';
 
 const burger = (props) => { 
+    // props : ingredients object [BurgerBuilder state]
+    const tab = [];
+    for (var obj in props.ingredients) {
+            for (var i = 0; i< props.ingredients[obj]; i++) { //props.ingredients[obj] = amount of Ingredience
+                tab.push(obj)
+            }}
+        const propsIngredients = tab.map((el, index) => <Ingredient key={index} type={el} />)
+    
     return(
         <div className={burgerStyling.Burger}>
             <Ingredient type="bread-top"/>
-            <Ingredient type="cheese"/>
-            <Ingredient type="meat"/>
+                {propsIngredients}
             <Ingredient type="bread-bottom"/>
         </div>
     );
 };
 
 export default burger;
+
+/* ES6 approach
+
+    const propsIngredients = Object.keys(props.ingredients).map(keyIng => {   //Object.keys (props.ingredients) = salad,bacon,cheese,meat    
+        return[...Array(props.ingredients[keyIng])].map((_, i) => {
+            return <Ingredient key={keyIng + i} type={keyIng} /> // key = "salad0", "salad1" ... 
+        });
+    });
+ */
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad:2,
+            bacon:1,
+            cheese:2,
+            meat:1
+        }
+    }
+
     render () {
         return (
             <React.Fragment>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </React.Fragment>
         );
