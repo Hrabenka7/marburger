e38dcd39d1322255667ed103defad55fb9f16d9b
Marketa Skoloudikova <marketa.skoloudikova@forcemanager.net>
2018-08-23 11:17:49 +0200

    burger controls styling and display

--- src/components/Burger/Controls/Controls.js
+++ src/components/Burger/Controls/Controls.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import controlsStyling from './Controls.css'
 
+import SingleControl from './SingleControl/SingleControl'
+
+
+const controls = [
+    { label: 'Salad', type: 'salad'},
+    { label: 'Bacon', type: 'bacon'},
+    { label: 'Cheese', type: 'cheese'},
+    { label: 'Meat', type: 'meat'}
+]
+
+
 const Controls = () => {
     return (
-        <div>
-            
+        <div className={controlsStyling.Controls}>
+            {controls.map(currentEl => (
+                <SingleControl key={currentEl.label} label={currentEl.label}/>
+            ))}
         </div>
     );
 };
