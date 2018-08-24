145f0ea7a5356d4c30a7a8368cb8835a3e4043d2
Marketa Skoloudikova <marketa.skoloudikova@forcemanager.net>
2018-08-24 14:30:23 +0200

    improving performance, update [OrderSummary] only if modal is shown

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import summaryStyling from './OrderSummary.css'
 
-const orderSummary = (props) => {
-    const ingredientsSummary = Object.keys(props.ingredients)
-                                .map(igKey => {
-                                    return  <li key={igKey}>
-                                                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                                            </li>
-                                })
-    return (
-        <React.Fragment>
-            <h3>Your Order</h3>
-            <p>Delitious marburger enhanced with: </p>
-            <ul>
-                {ingredientsSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong> </p>
-            <p>Continue to check out?</p>
-            <button className={[summaryStyling.Button, summaryStyling.Danger].join(' ')} onClick={props.purchaseCanceled}>CANCEL</button>
-            <button className={[summaryStyling.Button, summaryStyling.Success].join(' ')} onClick={props.purchaseContinue}>CONTINUE</button>
-        </React.Fragment>
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate')
+    }
+
+    render() {
+        const ingredientsSummary = Object.keys(this.props.ingredients)
+        .map(igKey => {
+            return  (
+                <li key={igKey}>
+                     <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                </li>
+           );
+        } 
     );
+    
+        return (
+            <React.Fragment>
+                <h3>Your Order</h3>
+                <p>Delitious marburger enhanced with: </p>
+                <ul>
+                    {ingredientsSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong> </p>
+                <p>Continue to check out?</p>
+                <button className={[summaryStyling.Button, summaryStyling.Danger].join(' ')} onClick={this.props.purchaseCanceled}>CANCEL</button>
+                <button className={[summaryStyling.Button, summaryStyling.Success].join(' ')} onClick={this.props.purchaseContinue}>CONTINUE</button>
+            </React.Fragment>
+        )
+    }
+
+    
 };
 
-export default orderSummary;
+export default OrderSummary;
 
